cadeefa2e8cf76dcbcfcd55a1f931995bcf28de3
HomnixMirn <vitalik.999f9cfe@gmail.com>
2025-12-10 13:06:41 +0400

    создание бд

--- Backend/back/models.py
+++ Backend/back/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 Запуск бека
-утсановите python 3.12 и выше
-py -m venv .venv
-.venv\Scripts\activate
-pip install django
-cd Backend
-py manage.py runserver
-
+1. утсановите python 3.12 и выше
+2. py -m venv .venv
+3. .venv\Scripts\activate
+4. pip install django
+5. pip install Pillow
+6. cd Backend
+7. py manage.py runserver
+8. Пароль от админ панель и логин admin/admin
 Запуск фронта
-Запустить ещё один терминал
-Установить node.js
-cd Frontend
-npm i
-npm run dev
+1. Запустить ещё один терминал
+2. Установить node.js
+3. cd Frontend
+4. npm i
+5. npm run dev
