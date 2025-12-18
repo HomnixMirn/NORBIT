7d3f8a76a71eb8b8342d91ee403859490c977d5b
HomnixMirn <vitalik.999f9cfe@gmail.com>
2025-12-19 00:03:17 +0400

    -Гирлянда

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import SnowBackground from '@/components/SnowBackground'
-import Garland from '@/components/Garland'
 
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
       <SnowBackground />
-        <Garland />
           <section className="content">
             
           </section>
